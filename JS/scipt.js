5586a48d3d777a0ca0accf4e603243a6a6df5a96
piel berekhah <victorpaul.tg01@gmail.com>
2021-03-04 22:54:04 +0300

    add all files

---
+++ JS/scipt.js
@@ -0,0 +1,21 @@
+
+$(document).ready(function () {
+  $(".designClick").click(function () {
+    $(".designVisible").toggle();
+    $(".designHidden").toggle();
+  });
+});
+
+$(document).ready(function () {
+  $(".developmentClick").click(function () {
+    $(".developmentVisible").toggle();
+    $(".developmentHidden").toggle();
+  });
+});
+
+$(document).ready(function() {
+    $(".productManagementClick").click(function() {
+        $(".productManagementVisible").toggle();
+        $(".productManagementHidden").toggle();
+    })
+})
