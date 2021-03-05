0ea08e710b501394016fdfcd85c1f9aba871555b
piel berekhah <victorpaul.tg01@gmail.com>
2021-03-05 11:44:10 +0300

    add all files

--- JS/scipt.js
+++ JS/scipt.js
@@ -13,9 +13,22 @@ $(document).ready(function () {
   });
 });
 
-$(document).ready(function() {
-    $(".productManagementClick").click(function() {
-        $(".productManagementVisible").toggle();
-        $(".productManagementHidden").toggle();
-    })
-})
+$(document).ready(function () {
+  $(".productManagementClick").click(function () {
+    $(".productManagementVisible").toggle();
+    $(".productManagementHidden").toggle();
+  });
+});
+$(document).ready(function () {
+  $("#contactForm").submit(function (e) {
+    e.preventDefault();
+    let name = $("input#name").val();
+    Swal.fire({
+      icon: "success",
+      text: `Hey ${name}, We will respond to your message as soon as possible. Thank you`,
+    });
+    $("#contactForm").each(function () {
+      this.reset();
+    });
+  });
+});
